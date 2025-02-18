1038c4df0c1e73c2dfe0e3d4ba4e9a0a54092fa2
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-18 16:03:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,9 @@ terraform {
     region = "ap-south-1"
   }
 }
+data "aws_route53_zone" "main" {
+  name = "cloudwithsunil.xyz"
+}
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
@@ -84,7 +87,13 @@ resource "aws_instance" "application" {
 resource "aws_eip" "app_eip" {
   instance = aws_instance.application.id
 }
-
+resource "aws_route53_record" "app_record" {
+  zone_id = data.aws_route53_zone.main.zone_id  # Refer to the existing hosted zone resource
+  name    = "www.sunilwithcloud.xyz"          # Replace with your subdomain name
+  type    = "A"
+  ttl     = "300"
+  records = [aws_eip.application_eip.public_ip]
+}
 output "eip_address" {
   value = aws_eip.app_eip.public_ip
 }
