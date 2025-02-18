e6827b94c16b5cad46f01bd9b2e30636441dbe8d
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-18 12:06:00 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key" {
-  description = "The AWS access key"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "The AWS secret key"
-  type        = string
-}
