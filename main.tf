1fb292c1b9cf440531caf0c25cfa0799c7346660
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-18 12:08:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "ap-south-1a"
 }
 
 resource "aws_security_group" "allow_web" {
