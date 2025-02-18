81e6dd8fa4bb95210797d7fec71db1bfe84846a3
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-18 12:47:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,12 @@ resource "aws_security_group" "allow_web" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
